c55c8412c8df44bae1d5dff5e566a36c1d8aca3b
Parmesh Mathur <parmeshkmathur@gmail.com>
2022-06-30 16:07:11 +0530

    final changes

--- Packet_tracer/packet_tracer.c
+++ Packet_tracer/packet_tracer.c
@@ -15,6 +15,11 @@
 #define IFNAMSIZ IF_NAMESIZE
 #define XT_TABLE_MAXNAMELEN   32
 
+#define ROUTE_EVT_IF 1
+#define ROUTE_EVT_IPTABLE 2
+
+int counter;
+
 int print_libbpf_log(enum libbpf_print_level lvl, const char *fmt, va_list args)
 {
     if (lvl >= LIBBPF_DEBUG)
@@ -36,35 +41,41 @@ static __u64 cnt;
 static void print_bpf_output(void *ctx, int cpu, void *data, __u32 size)
 {
 	struct route_evt_t {
-    		__u64 flags;
+		__u64 flags;
 
-    		char ifname[IFNAMSIZ];
-    		__u64 netns;
-
-    		__u64 ip_version;
-    		__u8 ipproto;
-    		__u64 saddr[2];
-    		__u64 daddr[2];
-    		__u64 fib_res_addr[2];
-    		unsigned char fib_res_prefixlen;
-    		unsigned char fib_res_type;
-
-    		__u32 pid;
-	   		__u64 hook;
-    		__u64 verdict;
-    		char tablename[XT_TABLE_MAXNAMELEN];
+		char ifname[IFNAMSIZ];
+		__u64 netns;
+		__u64 pack_len;
+		__u64 ip_version;
+		__u8 l4proto;
+		__u64 saddr[2];
+		__u64 daddr[2];
+		__u64 fib_res_addr[2];
+		unsigned char fib_res_prefixlen;
+		unsigned char fib_res_type;
+		__u64 pkt_type;
+		__u32 pid;
+		__u64 hook;
+		__u64 verdict;
+		char tablename[XT_TABLE_MAXNAMELEN];
+		char processname[16];
 	};
 	struct route_evt_t* e = (struct route_evt_t*)data;
+	// if ((e->flags & ROUTE_EVT_IF) != ROUTE_EVT_IF)
+	// 	return;
+
+	// if(e->l4proto==1)
+	// 	return;
 
 	struct in_addr sip_addr;
-    	sip_addr.s_addr = e->saddr[0];
+    sip_addr.s_addr = e->saddr[0];
 
 	struct in_addr dip_addr;
-        dip_addr.s_addr = e->daddr[0];
-
-	printf("\n%*u | %*s | %*llu | %*llu | %*llu | %*s | %*s | %llu | %s", -6, e->pid, 10, e->ifname, -6, e->flags, -5, e->netns, -10, e->ip_version, -18, inet_ntoa(sip_addr), -16, inet_ntoa(dip_addr), e->hook, e->tablename);
+    dip_addr.s_addr = e->daddr[0];
 
 	cnt++;
+	printf("\n%*llu | %*u | %*s | %*s | %*llu | %*llu | %*s | ", 5,  cnt, -6, e->pid, 16, e->processname, 10, e->ifname, -5, e->pack_len, -5, e->ip_version, -16, inet_ntoa(sip_addr));
+	printf("%*s | %*u ", -16, inet_ntoa(dip_addr), -6, e->l4proto);
 
 	if (cnt == MAX_CNT) {
 		printf("recv %lld events per sec\n",
@@ -77,7 +88,7 @@ void trace_reader()
 {
 	int trace_fd = open("/sys/kernel/debug/tracing/trace_pipe", O_RDONLY, 0);
 	if(trace_fd<0) return;
-	// TODO: make a map fd and read from that instead of from trace pipe
+
 	int count = 0;
 
 	while(1)
@@ -100,13 +111,14 @@ void trace_reader()
 int main(int argc, char **argv)
 {
 	struct capture_bpf *obj;
-	struct perf_buffer *pb = NULL;
+	struct perf_buffer *pb;
 
 	int err = 0;
+	counter = 0;
 
 	int map_fd, ret = 0;
 
-	char titles[][13] = {"PID", "IF NAME", "FLAGS", "NETNS", "IP VERSION", "SRC ADDRESS", "DEST ADDRESS", "HOOK", "T_NAME"};
+	char titles[][13] = {"PID", "IF NAME", "FLAGS", "PLEN", "IP_V", "SRC ADDRESS", "DST ADDRESS", "HOOK", "T_NAME", "PROTO", "S_NO", "COMMAND"};
 
 	struct rlimit rlim = {
 		.rlim_cur = 512UL << 20,
@@ -135,8 +147,8 @@ int main(int argc, char **argv)
 
     map_fd = bpf_map__fd(obj->maps.route_evt);
     if (map_fd < 0) {
-            fprintf(stderr, "ERROR: finding a map in obj file failed\n");
-            goto cleanup;
+		fprintf(stderr, "ERROR: finding a map in obj file failed\n");
+		goto cleanup;
     }
 
 	err = capture_bpf__attach(obj);
@@ -152,26 +164,21 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
-	printf("\n%*s | %*s | %*s | %*s | %*s | %*s  | %*s | %s | %s", -6, titles[0], 10, titles[1], -6, titles[2], -5, titles[3], -10, titles[4], -17, titles[5], -15, titles[6], titles[7], titles[8]);
-
+	printf("\n%*s | %*s | %*s | %*s | %*s | %*s | %*s | %*s | %*s", 5, titles[10], -6, titles[0], 16, titles[11], 10, titles[1], -5, titles[3], -5, titles[4], -16, titles[5], -16, titles[6], -6, titles[9]);
+/*
+ * If the kernel space code is usiing printk statements to debug,
+ * Comment out the above print statement and the while statement below.
+ * uncomment the trace_reader() function to dsiplay the data that
+ * bpf_printk outputs to the trace_pipe file.
+ */
 	start_time = time_get_ns();
-<<<<<<< HEAD
-	// while ((ret = perf_buffer__poll(pb, 1000)) >= 0 && cnt < MAX_CNT) {
-	// }
-	// kill(0, SIGINT);
-	trace_reader();
-=======
-
 	while ((ret = perf_buffer__poll(pb, 1000)) >= 0 && cnt < MAX_CNT) {
 	}
 	kill(0, SIGINT);
->>>>>>> 2788d5f736311d220324b11f8d70facdfbf82987
+	// trace_reader();
+
 
 cleanup:
-	if(!pb) 
-	{
-		perf_buffer__free(pb);
-	}
 	capture_bpf__destroy(obj);
 	return err != 0;
 }
